b86d779bb4b6ada53cb174d0b210804a0618189a
chang_su <chang_su@hunliji.com>
2016-09-19 13:46:25 +0800

    first helloworld vue

---
+++ components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="app">
+       hello world
+    </div>
+</template>
+
+<script>
+
+
+    export default {
+        components: {
+
+        }
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: __dirname,
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.vue?$/,
+      exclude: /node_modules/,
+      loader: 'vue'
+    },{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    },{
+      test: /\.scss$/,
+
+      loader: "style!css!postcss!sass"
+    },{
+      test: /\.css$/,
+
+      loader: "style!css!postcss"
+    },{
+      test: /\.css$/,
+      include: /node_modules/,
+      loader: 'style!css'
+    }]
+  },
+  babel: {
+    presets: ['es2015'],
+    plugins: ['transform-runtime']
+  },
+  postcss: [
+    require('precss')(),
+    require('autoprefixer')({browsers: ['last 2 versions','IE 9']}),
+
+  ],
+  vue: {
+    loaders: {
+      js: 'babel',
+      css: 'vue-style!css!postcss',
+      less:'vue-style!css!postcss!less',
+      scss:'vue-style!css!postcss!sass'
+    }
+  },
+}
