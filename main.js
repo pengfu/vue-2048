0cfe2db102e66d6a1292df8882ab4fa562012ac3
chang_su <chang_su@hunliji.com>
2017-07-06 15:34:58 +0800

    uggrade to vue2

--- main.js
+++
@@ -1,9 +0,0 @@
-import './src/assets/scss/style.scss'
-import './src/assets/scss/main.scss'
-import Vue from 'vue'
-import BoardView from './src/components/BoardView.vue'
-
-new Vue({
-  el: '#boardDiv',
-  components: { BoardView }
-})
